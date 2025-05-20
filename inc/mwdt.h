d957a34f4d4b78771088715bd0d8b524180eec8f
ñoquis <leonzg4231@gmail.com>
2025-05-20 16:38:53 -0300

    commiting whatever i didn't save

--- inc/mwdt.h
+++ inc/mwdt.h
@@ -1,9 +1,9 @@
-#ifndef _MAGIC_WORKING_DWARF_THREAD_DEFINED_
-#define _MAGIC_WORKING_DWARF_THREAD_DEFINED_
+#ifndef _MAGIC_WORKING_GNOME_THREAD_DEFINED_
+#define _MAGIC_WORKING_GNOME_THREAD_DEFINED_
 
 #include "headers.h"
 
-struct dwarf_attribs_t
+struct gnome_attribs_t
 {
     uint16_t q : 4;
     uint16_t health : 2;
@@ -12,40 +12,40 @@ struct dwarf_attribs_t
 
 } __attribute__((packed));
 
-struct dwarf_t
+struct gnome_t
 {
     uint16_t x;
     uint16_t y;
 
     uint16_t active_task;
-    dwarf_attribs_t attribs;
+    gnome_attribs_t attribs;
 
 } __attribute__((packed));
 
-struct dwarf_team_t
+struct gnome_team_t
 {
     uint32_t team_size;
-    dwarf_t **dwarf_buffer;
+    gnome_t **gnome_buffer;
 
     uint16_t target_x;
     uint16_t target_y;
 }__attribute__((packed));
 
-struct dwarf_batch_t
+struct gnome_batch_t
 {
-dwarf_t batch_items[4096];
-dwarf_batch_t*next;
+gnome_t batch_items[4096];
+gnome_batch_t*next;
 }__attribute__((packed));
 
-struct dwarf_buffer_t
+struct gnome_buffer_t
 {
     uint16_t id;
-    uint32_t dwarf_count;
-    dwarf_batch_t *dwarf_buffer;
+    uint32_t gnome_count;
+    gnome_batch_t *gnome_buffer;
 };
 
 
-dwarf_buffer_t gen_dwarf_buffer(uint32_t size);
+gnome_buffer_t gen_gnome_buffer(uint32_t size);
 
 
 
