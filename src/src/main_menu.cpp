beff299fdf03f15619635e86a2cc81ab7b8db5bf
ñoquis <leonzg4231@gmail.com>
2025-05-23 17:02:40 -0300

    lol

--- src/src/main_menu.cpp
+++ src/src/main_menu.cpp
@@ -2,8 +2,6 @@
 #include <window.h>
 #include <ui.h>
 
-static sbutton_t play_b;
-
 inline void handle_events()
 {
     ui_frame();
@@ -15,28 +13,20 @@ inline void handle_events()
     }
 }
 
-void aaaaaA()
-{
-printf("AAAAAAAAAAA\n");
-}
 
 void main_menu_init()
 {
-    play_b.onrelease = aaaaaA;
-    play_b.rect={40,40,256,128};
+
 }
 
 void main_menu_update()
 {
     handle_events();
 
-    handle_sbutton(&play_b);
+    
 
     SDL_SetRenderDrawColor(window_hnd.renderer, 0, 0, 0, 255);
     SDL_RenderClear(window_hnd.renderer);
-
-    SDL_SetRenderDrawColor(window_hnd.renderer, 255, 0, 0, 255);
-    SDL_RenderFillRect(window_hnd.renderer,&play_b.rect);
 
     SDL_RenderPresent(window_hnd.renderer);
 }
